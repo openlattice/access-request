50eedc291d99eee871fc477c8c169c4b675f145c
Solomon Tang <solomoncztang@gmail.com>
2021-02-16 06:08:08 -0800

    route utils

---
+++ src/utils/RouteUtils.js
@@ -0,0 +1,15 @@
+// @flow
+import type { Match } from 'react-router';
+
+const getRelativeRoot = (root :string, match :Match) => {
+  if (match.params) {
+    return Object.entries(match.params)
+      // $FlowFixMe
+      .reduce((newRoot, [param, value]) => newRoot.replace(RegExp(`:${param}`, 'g'), value), root);
+  }
+  return root;
+};
+
+export {
+  getRelativeRoot
+};
