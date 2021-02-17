625b33ebada265481423ef73f0483009327c9ce3
Solomon Tang <solomoncztang@gmail.com>
2021-02-16 22:42:37 -0800

    surface selectors

--- src/core/redux/selectors/index.js
+++ src/core/redux/selectors/index.js
@@ -6,6 +6,7 @@ import { ReduxUtils } from 'lattice-utils';
 
 export { default as selectPropertyTypeIDsByFQN } from './selectPropertyTypeIDsByFQN';
 export { default as selectAccessHits } from './selectAccessHits';
+export { default as selectAccessRequestData } from './selectAccessRequestData';
 
 export const {
   selectEntitySets,
