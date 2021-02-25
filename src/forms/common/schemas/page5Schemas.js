bb2bb828507812df310ee4ef79be8e51be035133
Solomon Tang <solomoncztang@gmail.com>
2021-02-24 21:28:21 -0800

    remove comment

--- src/forms/common/schemas/page5Schemas.js
+++ src/forms/common/schemas/page5Schemas.js
@@ -9,29 +9,6 @@ export const page5Schema = {
       description: 'Note: financial interests include but are not limited to:\n• Present or anticipated ownership of stock, stock options, or other financial obligations of the source of funding.\n• Receipt or expectation of payment of any sort in connection with papers, symposia, consulting, editing, etc. from the source of funding.\n• The sale or licensing or anticipated sale or licensing of medical or other products or intellectual property, such as patents, copyrights, or trade secrets to the source of funding or other entities.\n• Any past, present or anticipated receipt of money or other valuable consideration from the source of research funding by the researcher(s), the family of the researcher(s), the research institution, or by an institution in which the researcher(s) or the family of the researcher(s) has an interest as owner, creditor, or officer.',
       enumNames: ['Yes', 'No'],
     },
-    
-    // interestedIndividuals: {
-    //   type: 'array',
-    //   title: 'Interested Individuals',
-    //   items: {
-    //     type: 'object',
-    //     properties: {
-    //       firstName: {
-    //         type: 'string',
-    //         title: 'First Name',
-    //       },
-    //       lastName: {
-    //         type: 'string',
-    //         title: 'Last Name',
-    //       },
-    //       conflictDescription: {
-    //         type: 'string',
-    //         title: 'Conflict Description',
-    //       },
-    //     },
-    //     default: [{}]
-    //   },
-    // },
   },
   dependencies: {
     conflictOfInterest: {
