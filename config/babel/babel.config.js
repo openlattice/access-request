799e8e7c020e9ec559edaa807fa2ba10b5811512
Hristo Oskov <hristo@openlattice.com>
2021-02-26 13:28:56 -0800

    webpack v5, babel updates, bump dependency versions

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime',
-    'babel-plugin-styled-components',
+    ['@babel/plugin-transform-runtime', {
+      corejs: 3,
+    }],
     ['babel-plugin-transform-imports', {
+      '@fortawesome/free-solid-svg-icons': {
+        transform: (importName) => `@fortawesome/free-solid-svg-icons/${importName}`,
+        preventFullImport: true,
+        skipDefaultConversion: true,
+      },
       '@fortawesome/pro-duotone-svg-icons': {
         preventFullImport: true,
         skipDefaultConversion: true,
@@ -25,13 +29,18 @@ module.exports = {
         skipDefaultConversion: true,
         transform: (importName) => `@fortawesome/pro-solid-svg-icons/${importName}`,
       },
-    }]
+    }],
+    'babel-plugin-styled-components',
   ],
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-flow',
+    ['@babel/preset-env', {
+      corejs: '3.9',
+      targets: ['defaults', 'not ie > 0'],
+      useBuiltIns: 'usage',
+    }],
     ['@babel/preset-react', {
       runtime: 'automatic'
-    }]
+    }],
+    '@babel/preset-flow',
   ],
 };
--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -30,10 +30,8 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({
         favicon: `${APP_PATHS.ABS.SOURCE_ASSETS}/svg/icons/ol-icon.svg`,
-        inject: true,
         template: `${APP_PATHS.ABS.SOURCE}/index.html`,
       }),
-      new Webpack.HotModuleReplacementPlugin(),
       ...baseConfig.plugins
     ],
   };
