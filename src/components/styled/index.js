d7bb4b214218f116537799f528e68905132074c3
Solomon Tang <solomoncztang@gmail.com>
2021-03-11 19:46:48 -0800

    add modal body

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,3 +1,6 @@
+// @flow
+import type { ComponentType } from 'react';
+
 import styled from 'styled-components';
 import { Form } from 'lattice-fabricate';
 
@@ -5,6 +8,17 @@ const EdgelessForm = styled(Form)`
   margin: -30px;
 `;
 
+const ModalBody :ComponentType<{|
+  children ?:any;
+  width ?:string;
+|}> = styled.div`
+  max-width: 100%;
+  width: ${({ width }) => ((typeof width === 'number' && width > 0) ? width : 720)}px;
+`;
+
+export default ModalBody;
+
 export {
-  EdgelessForm
+  EdgelessForm,
+  ModalBody,
 };
