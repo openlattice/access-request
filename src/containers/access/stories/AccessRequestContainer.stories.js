9256b79674013f5b372eec6478e0d8b0f5e848e6
Solomon Tang <solomoncztang@gmail.com>
2021-03-23 17:48:08 -0700

    refactor action request attachment button

--- src/containers/access/stories/AccessRequestContainer.stories.js
+++ src/containers/access/stories/AccessRequestContainer.stories.js
@@ -23,8 +23,8 @@ const Template = (args) => {
 
 export const Live = Template.bind({});
 Live.args = {
-  jwt: '123',
-  organizationId: '00000000-0000-0000-0000-000000000000',
+  jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbG9tb25Ab3BlbmxhdHRpY2UuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInVzZXJfaWQiOiJnb29nbGUtb2F1dGgyfDExMTIxNzkwNTcyOTE4NzM3ODczNCIsInVzZXJfbWV0YWRhdGEiOnt9LCJhcHBfbWV0YWRhdGEiOnsicm9sZXMiOlsiQXV0aGVudGljYXRlZFVzZXIiLCJhZG1pbiJdLCJvcmdhbml6YXRpb25zIjpbIjAwMDAwMDAwLTAwMDAtMDAwMS0wMDAwLTAwMDAwMDAwMDAwMCJdLCJhY3RpdmF0ZWQiOiJhY3RpdmF0ZWQifSwibmlja25hbWUiOiJzb2xvbW9uIiwicm9sZXMiOlsiQXV0aGVudGljYXRlZFVzZXIiLCJhZG1pbiJdLCJpc3MiOiJodHRwczovL29wZW5sYXR0aWNlLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMTIxNzkwNTcyOTE4NzM3ODczNCIsImF1ZCI6Im84WTJVMnpiNUl3bzAxamR4TU4xVzJhaU44UHh3VmpoIiwiaWF0IjoxNjE2MDg2Njk1LCJleHAiOjE2MTYxMjI2OTV9.fvXK1TlMq9t5se7hH80SgakUPFoJHW5nsOyDFmUL-FM',
+  organizationId: '1d5aa1f4-4d22-46a5-97cd-dcc6820e7ff8',
   match: {},
   root: '/'
 };
