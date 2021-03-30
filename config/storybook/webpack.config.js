5a3242dee3255619541d0e30784c0fa40d25a7e3
Hristo Oskov <hristo@openlattice.com>
2021-03-29 18:05:11 -0700

    update comment

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = ({ config }) => {
 
   config.resolve.alias = {
     ...config.resolve.alias,
-    // NOTE: because rjsf still depends on core-js 2
+    // NOTE: because rjsf still depends on core-js@2
     // core-js-pure is the core-js@3 equivalent of core-js/library from core-js@2
     // https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md
     'core-js/library/fn/array/fill': path.resolve(NODE, 'core-js-pure/features/array/fill'),
