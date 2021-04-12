9c33308d7df7581a59d9528da177fe5e990a9a58
Solomon Tang <solomoncztang@gmail.com>
2021-04-12 10:06:02 -0700

    add AttachmentsModal tests

---
+++ src/containers/access/src/tests/AttachmentsModal.test.js
@@ -0,0 +1,84 @@
+import toJson from 'enzyme-to-json';
+import { mount } from 'enzyme';
+import { Modal } from 'lattice-ui-kit';
+import { NIL } from 'uuid';
+
+import AttachmentsBody from '../AttachmentsBody';
+import AttachmentsModal from '../AttachmentsModal';
+import ModuleProvider from '../../../../core/provider/ModuleProvider';
+
+const mockDispatch = jest.fn();
+const mockUseDispatch = jest.fn();
+mockUseDispatch.mockReturnValue(mockDispatch);
+
+jest.mock('../../../../core/redux', () => {
+  const {
+    useSelector,
+    useStore,
+    moduleStore,
+    moduleContext,
+  } = jest.requireActual('../../../../core/redux');
+  return {
+    moduleContext,
+    moduleStore,
+    useDispatch: () => mockUseDispatch(),
+    useSelector,
+    useStore,
+  };
+});
+
+describe('AttachmentsModal', () => {
+
+  test('match snapshot when closed', () => {
+
+    const wrapper = mount(
+      <ModuleProvider>
+        <AttachmentsModal accessRequestId={NIL} />
+      </ModuleProvider>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('match snapshot when open', () => {
+
+    const wrapper = mount(
+      <ModuleProvider>
+        <AttachmentsModal accessRequestId={NIL} isVisible />
+      </ModuleProvider>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('render modal', () => {
+    const wrapper = mount(
+      <ModuleProvider>
+        <AttachmentsModal accessRequestId={NIL} isVisible />
+      </ModuleProvider>
+    );
+    const modalWrapper = wrapper.find(Modal);
+    expect(modalWrapper).toHaveLength(1);
+  });
+
+  test('render AttachmentsBody as Modal child', () => {
+    const wrapper = mount(
+      <ModuleProvider>
+        <AttachmentsModal accessRequestId={NIL} isVisible />
+      </ModuleProvider>
+    );
+    const modalWrapper = wrapper.find(Modal);
+    expect(modalWrapper.contains(AttachmentsBody)).toEqual(true);
+  });
+
+  test('pass accessRequestId to AttachmentsBody', () => {
+    const wrapper = mount(
+      <ModuleProvider>
+        <AttachmentsModal accessRequestId={NIL} isVisible />
+      </ModuleProvider>
+    );
+
+    const attachmentsBodyWrapper = wrapper.find(AttachmentsBody);
+    expect(attachmentsBodyWrapper.prop('accessRequestId')).toEqual(NIL);
+
+  });
+
+});
