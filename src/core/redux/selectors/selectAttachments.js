aaff9cb52e04467e6fe39b6dc7076b3a8ae439d8
Solomon Tang <solomoncztang@gmail.com>
2021-04-12 18:32:33 -0700

    update function name

--- src/core/redux/selectors/selectAttachments.js
+++ src/core/redux/selectors/selectAttachments.js
@@ -1,9 +1,9 @@
 // @flow
-import { List, Map } from 'immutable';
+import { Map } from 'immutable';
 
 import { ATTACHMENTS } from '../../../containers/access/src/reducers/constants';
 import { ACCESS } from '../constants';
 
-export default function selectAccessHits() {
-  return (state :Map) :List => state.getIn([ACCESS, ATTACHMENTS], List());
+export default function selectAttachments() {
+  return (state :Map) :Map => state.getIn([ACCESS, ATTACHMENTS], Map());
 }
