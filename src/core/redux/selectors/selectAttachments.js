999848b00787859b7d1c8cd8678a2c90e111e66d
Solomon Tang <solomoncztang@gmail.com>
2021-04-14 05:02:52 -0700

    Update src/core/redux/selectors/selectAttachments.js
    Co-authored-by: Hristo Oskov <UnsungHero97@users.noreply.github.com>

--- src/core/redux/selectors/selectAttachments.js
+++ src/core/redux/selectors/selectAttachments.js
@@ -5,5 +5,5 @@ import { ATTACHMENTS } from '../../../containers/access/src/reducers/constants';
 import { ACCESS } from '../constants';
 
 export default function selectAttachments() {
-  return (state :Map) :Map => state.getIn([ACCESS, ATTACHMENTS], Map());
+  return (state :Map) :Map => state.getIn([ACCESS, ATTACHMENTS]) || Map();
 }
